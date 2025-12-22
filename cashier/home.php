07499a486f2614afd6c3bd28caa2a01eacddaf14
ronniestack <ronniecalleda08@gmail.com>
2025-12-22 21:47:24 +0800

    improve code structure and logic, add sweetalert 2 toast

--- cashier/home.php
+++ cashier/home.php
@@ -1,86 +1,136 @@
 <div class="container">
-    <div class="row">
-        <div class="col-md-6 d-flex flex-column justify-content-center align-items-center" id="serving-field">
+    <div class="row g-4">
 
-            <div class="card col-sm-8 shadow">
-                <div class="card-header">
-                    <h5 class="card-title text-center">Now Serving</h5>
+        <!-- NOW SERVING -->
+        <div class="col-md-6 d-flex align-items-center justify-content-center" id="serving-field">
+            <div class="card shadow-sm w-100" style="max-width:420px">
+                <div class="card-header bg-light text-center">
+                    <h5 class="mb-0 fw-semibold">Now Serving</h5>
                 </div>
-                <div class="card-body">
-                    <div class="fs-1  my-2 fw-bold text-center"><span id="queue">----</span></div>
-                </div>
-                <div class="card-footer">
-
+                <div class="card-body text-center py-5">
+                    <div class="display-3 fw-bold text-success" id="queue">----</div>
                 </div>
             </div>
         </div>
-        <div class="col-md-6 d-flex flex-column justify-content-center align-items-center" id="action-field">
-            <div class="w-100 row row-cols-sm-1 row-cols-md-2 row-cols-xl-3">
-                <div class="col">
-                    <button id="next_queue" class="btn btn-flat btn-primary rounded-0 btn-lg"><i class="fa fa-forward"></i> Next</button>
-                </div>
-                <div class="col">
-                    <button id="notify" class="btn btn-flat btn-secondary rounded-0 btn-lg"><i class="fa fa-bullhorn"></i> Notify</button>
+
+        <!-- ACTIONS -->
+        <div class="col-md-6 d-flex align-items-center justify-content-center" id="action-field">
+            <div class="w-100" style="max-width:420px">
+                <div class="row g-3">
+                    <div class="col-12">
+                        <button id="next_queue"
+                                class="btn btn-success btn-lg w-100">
+                            <i class="fa-solid fa-forward me-2"></i>
+                            Next Queue
+                        </button>
+                    </div>
+                    <div class="col-12">
+                        <button id="notify"
+                                class="btn btn-outline-secondary btn-lg w-100">
+                            <i class="fa-solid fa-bullhorn me-2"></i>
+                            Notify
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 <script>
-    var websocket = new WebSocket("ws://<?php echo $_SERVER['SERVER_NAME'] ?>:2306/queuing/php-sockets.php"); 
-    websocket.onopen = function(event) { 
-      console.log('socket is open!')
-		}
-    websocket.onclose = function(event){
-      console.log('socket has been closed!')
-    var websocket = new WebSocket("ws://<?php echo $_SERVER['SERVER_NAME'] ?>:2306/queuing/php-sockets.php"); 
+    var websocket = new WebSocket("ws://127.0.0.1:2306");
+
+    websocket.onopen = function () {
+        console.log('socket is open!');
     };
+
+    websocket.onclose = function () {
+        console.log('socket has been closed!');
+        // optional auto-reconnect
+        setTimeout(() => {
+            websocket = new WebSocket("ws://127.0.0.1:2306");
+        }, 1000);
+    };
+
     var in_queue = {};
+
     function _resize_elements(){
-        var window_height = $(window).height()
-        var nav_height = $('#topNavBar').height()
-        var container_height = window_height - nav_height
-        $('#serving-field,#action-field').height(container_height - 50)
+        var window_height = $(window).height();
+        var nav_height = $('#topNavBar').height();
+        $('#serving-field,#action-field').height(window_height - nav_height - 50);
     }
+
     function get_queue(){
         $.ajax({
             url:'./../Actions.php?a=next_queue',
             dataType:'json',
-            error:err=>console.log(err),
+            error: err => {
+                console.error(err);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'An error occurred while fetching the queue.'
+                });
+            },
             success:function(resp){
                 if(resp.status){
                     if(Object.keys(resp.data).length > 0){
-                        in_queue = resp.data
+                        in_queue = resp.data;
+                        queue();
                     }else{
-                        in_queue = {}
-                        alert("No Queue Available")
+                        in_queue = {};
+                        Swal.fire({
+                            icon: 'info',
+                            title: 'No Queue',
+                            text: 'No queue available at the moment.',
+                            timer: 2000,
+                            showConfirmButton: false
+                        });
+                        queue();
                     }
                 }else{
-                    alert('An error occured')
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'An error occurred.'
+                    });
                 }
-                queue();
             }
-        })
+        });
+    }
 
-    }
     function queue(){
-        $('#queue').text(in_queue.queue || "----")
-        websocket.send(JSON.stringify({type:'queue',cashier_id:'<?php echo $_SESSION['cashier_id'] ?>',qid:in_queue.queue_id}))
+        $('#queue').text(in_queue.queue || "----");
+        if(in_queue.queue){
+            websocket.send(JSON.stringify({
+                type: 'queue',
+                cashier_id: '<?php echo $_SESSION['cashier_id'] ?>',
+                qid: in_queue.queue_id
+            }));
+        }
     }
+
     _resize_elements();
+
     $(function(){
-        $(window).resize(function(){
-            _resize_elements()
-        })
+        $(window).resize(_resize_elements);
+
         $('#next_queue').click(function(){
-            get_queue()
-        })
+            get_queue();
+        });
+
         $('#notify').click(function(){
             if(!!in_queue.queue){
-                queue()
+                queue();
             }else{
-                alert("No Queue Available")
+                Swal.fire({
+                    icon: 'info',
+                    title: 'No Queue',
+                    text: 'No queue available to notify.',
+                    timer: 2000,
+                    showConfirmButton: false
+                });
             }
-        })
-    })
+        });
+    });
 </script>
